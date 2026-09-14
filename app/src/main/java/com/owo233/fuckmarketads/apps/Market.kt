fb993c98efb308c462f440eac3d826176d4f1417
Mars <34854875+moonbai@users.noreply.github.com>
2026-09-14 08:53:49 +0800

    更新 Market.kt

--- app/src/main/java/com/owo233/fuckmarketads/apps/Market.kt
+++ app/src/main/java/com/owo233/fuckmarketads/apps/Market.kt
@@ -6,7 +6,6 @@ import com.owo233.fuckmarketads.hooks.market.RemoveAds
 import com.owo233.fuckmarketads.hooks.market.TabFilter
 import com.owo233.fuckmarketads.init.AppPackage
 import com.owo233.fuckmarketads.init.AppRegister
-import com.owo233.fuckmarketads.HookSettings
 import io.github.libxposed.api.XposedModuleInterface
 
 object Market : AppRegister() {
@@ -15,11 +14,10 @@ object Market : AppRegister() {
         get() = AppPackage.MARKET
 
     override fun onPackageReady(param: XposedModuleInterface.PackageReadyParam) {
-        HookSettings.init(HookEnv.base)
         autoInitHooks(
             param,
             HideSecurityView,
-            TaFilter,
+            TabFilter,
             RemoveAds,
             MiscApply
         )
